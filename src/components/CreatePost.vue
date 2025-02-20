a5f793caf25d414cdcd971e03d2cb7543f79d8c5
wenchi7 <syz58171020@gmail.com>
2025-02-20 17:25:57 +0800

    add data in firebase && initialrize firebase

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -2,7 +2,7 @@
   <div class="create-view flex justify-center mt-10">
     <div class="text-center bg-pink-200 w-9/12 min-h-auto rounded-xl mb-10 pb-10">
       <div class="m-10 flex items-center">
-        <form class="flex w-full flex-col gap-4 relative">
+        <form class="flex w-full flex-col gap-4 relative" @submit.prevent="create">
           <div class="flex w-full">
             <label
               for="post-title"
@@ -10,6 +10,7 @@
               >Title :
             </label>
             <input
+              v-model="title"
               type="text"
               id="post-title"
               class="py-3 px-2 placeholder:px-2 rounded-lg flex w-full"
@@ -25,6 +26,7 @@
               >Prodact :
             </label>
             <input
+              v-model="product"
               type="text"
               id="product-name"
               class="py-3 px-2 placeholder:px-2 rounded-lg w-full"
@@ -40,6 +42,7 @@
               >description:</label
             >
             <textarea
+              v-model="description"
               name="post-content"
               id="post-content"
               class="p-3 rounded-lg w-full min-h-64 overflow-y-scroll"
@@ -48,6 +51,7 @@
           </div>
 
           <div>
+            <!-- 有image.url才顯示圖片,:src="O"讓O控制照片來源，讓圖片動態變化 -->
             <input type="file" @change="uploadImage" />
             <img v-if="imageUrl" :src="imageUrl" alt="Uploaded Image" />
           </div>
@@ -56,7 +60,7 @@
             <input
               type="submit"
               value="share"
-              class="border border-gray-900 px-2 py-1 rounded bg-amber-300"
+              class="border border-gray-900 px-2 py-1 rounded bg-amber-300 cursor-pointer"
             />
           </div>
         </form>
@@ -67,18 +71,25 @@
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
-
+import { db } from '@/backend/firebbase.js'
+import { collection, addDoc } from 'firebase/firestore'
+import { useRouter } from 'vue-router'
+const title = ref('')
+const product = ref('')
+const description = ref('')
 const imageUrl = ref('')
 const uploadImage = async (event) => {
+  // event.target指的是input type=file的元素,files是檔案的陣列，event.target.files[0]是要拿這個input裡的檔案列的第1個
   const file = event.target.files[0]
   console.log(event.target.files)
-
   if (!file) return
-
+  //如果沒有選擇任何檔案就結束函數
+  //不然就創建一個FormData物件並追加資料
   const formData = new FormData()
   formData.append('file', file)
   formData.append('upload_preset', 'wenchi_preset')
   formData.append('cloud_name', 'dvzkvj8cs')
+  //使用axios發送post請求並等待結果
   try {
     const response = await axios.post(
       `https://api.cloudinary.com/v1_1/dvzkvj8cs/image/upload`,
@@ -87,7 +98,45 @@ const uploadImage = async (event) => {
     console.log(response.data.secure_url)
     imageUrl.value = response.data.secure_url
   } catch (error) {
-    console.error('Upload failed:', error.response ? error.response.data : error.message)
+    console.error('Upload failed:', error.message)
   }
 }
+const router = useRouter()
+
+const create = async () => {
+  try {
+    const docRef = await addDoc(collection(db, 'posts'), {
+      title: title.value,
+      product: product.value,
+      description: description.value,
+      imageUrl: imageUrl.value,
+    })
+    console.log(docRef.id)
+    router.push('/home')
+  } catch (e) {
+    console.error('error message', e)
+  } finally {
+    title.value = ''
+    product.value = ''
+    description.value = ''
+    imageUrl.value = ''
+  }
+}
+
+// const submitPost = async () => {
+//   try {
+//     await addDoc(collection(db, 'posts'), {
+//       title: title.value,
+//       product: product.value,
+//       description: imageUrl.value,
+//     })
+//     ;(title.value = ''),
+//       (product.value = ''),
+//       (description.value = ''),
+//       (imageUrl.value = ''),
+//       alert('Post submitted!')
+//   } catch (error) {
+//     console.error('Error adding document:', error)
+//   }
+// }
 </script>
